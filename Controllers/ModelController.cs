d41a53c4696548b959039c4cce42ee7b346ae68d
Kasper <132390730+Kasper632@users.noreply.github.com>
2024-04-04 15:29:13 +0200

    add+see current models

--- Controllers/ModelController.cs
+++ Controllers/ModelController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using App.Entities;
+using App.Models;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using MongoDB.Entities;
 
 namespace App.Controllers
 {
@@ -6,7 +10,27 @@ namespace App.Controllers
 	{
 		public async Task<IActionResult> HandleModels()
 		{
-			return View();
+            //ViewBag.CurrentModels = Query.FetchAll<Model>();
+            var cursor = await DB.Find<Model>().ExecuteCursorAsync();
+            ViewBag.CurrentModels = await cursor.ToListAsync();
+
+            return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> AddNewModel(ModelViewModel modelViewModel)
+		{
+			Model model = new Model
+			{
+				ModelName = modelViewModel.ModelName,
+				Description = modelViewModel.Description,
+				Picture = modelViewModel.Picture,
+				ProductCode = modelViewModel.ProductCode
+			};
+
+			await model.SaveAsync();
+
+			return RedirectToAction("HandleModels", "Model");
 		}
 	}
 }
