711f6b8909dc3005839358d9547b9128ab52785c
Kasper <132390730+Kasper632@users.noreply.github.com>
2024-04-04 14:06:31 +0200

    beginning

---
+++ Controllers/ModelController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.Controllers
+{
+	public class ModelController : Controller
+	{
+		public async Task<IActionResult> HandleModels()
+		{
+			return View();
+		}
+	}
+}
---
+++ Models/ModelViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace App.Models
+{
+	public class ModelViewModel
+	{
+		public string ModelName { get; set; }
+		public string Description { get; set; }
+		public string Picture { get; set; }
+		public string ProductCode { get; set; }
+	}
+}
