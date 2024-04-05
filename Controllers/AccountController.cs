9916e15c78516dcd74366efbed458c815ede7c1d
LarssonMarcus <132892741+LarssonMarcus@users.noreply.github.com>
2024-04-05 13:15:48 +0200

    *loggain

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using App.Services;
+using System.Diagnostics;
+ using App.Models;
 
 namespace App.Controllers
 {
-    public class AccountController : Controller
+    public class AccountController (Authenticator authenticator) : Controller
     {
+        [HttpGet]
         public IActionResult Login()
         {
             return View();
@@ -12,5 +16,48 @@ namespace App.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model, String returnUrl)
+        {
+            try {
+                if (ModelState.IsValid)
+                {
+                    var username = model.userName;
+                    var password = model.password;
+                    if (await authenticator.SignInAsync(username, password))
+                    {
+                        if(returnUrl !=null)
+                        {
+                            return Redirect(returnUrl);
+                        }
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            
+                return View();     
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> LogOut ()
+        {
+            try
+            {
+                await authenticator.SignOutAsync();
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            return RedirectToAction ("Index","Home");
+        }
+
     }
+
 }
