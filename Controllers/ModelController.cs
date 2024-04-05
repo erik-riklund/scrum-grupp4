a9dbd4aeb306a98e09c6e53b29f6460a48ee19d0
Kasper <kasperv98@gmail.com>
2024-04-05 13:45:46 +0200

    edit and delete

--- Controllers/ModelController.cs
+++ Controllers/ModelController.cs
@@ -10,11 +10,11 @@ namespace App.Controllers
 	{
 		public async Task<IActionResult> HandleModels()
 		{
-            //ViewBag.CurrentModels = Query.FetchAll<Model>();
-            var cursor = await DB.Find<Model>().ExecuteCursorAsync();
-            ViewBag.CurrentModels = await cursor.ToListAsync();
+			//ViewBag.CurrentModels = Query.FetchAll<Model>();
+			var cursor = await DB.Find<Model>().ExecuteCursorAsync();
+			ViewBag.CurrentModels = await cursor.ToListAsync();
 
-            return View();
+			return View();
 		}
 
 		[HttpPost]
@@ -31,6 +31,64 @@ namespace App.Controllers
 			await model.SaveAsync();
 
 			return RedirectToAction("HandleModels", "Model");
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> EditModel(string modelID)
+		{
+			var model = await Query.FetchOneById<Model>(modelID);
+
+			if (model != null)
+			{
+				var modelViewModel = new ModelViewModel
+				{
+					ModelName = model.ModelName,
+					Description = model.Description,
+					Picture = model.Picture,
+					ProductCode = model.ProductCode
+				};
+
+				ViewBag.ModelID = model.ID;
+				return View(modelViewModel);
+			}
+			else
+			{
+				return NotFound();
+			}
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> SaveEdit(string modelID, ModelViewModel modelViewModel)
+		{
+			var model = await Query.FetchOneById<Model>(modelID);
+
+			if (model != null)
+			{
+				model.ModelName = modelViewModel.ModelName;
+				model.Description = modelViewModel.Description;
+				model.Picture = modelViewModel.Picture;
+				model.ProductCode = modelViewModel.ProductCode;
+				await model.SaveAsync();
+
+				return RedirectToAction("HandleModels", "Model");
+			}
+
+			return NotFound();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> DeleteModel(string modelID)
+		{
+			var model = await Query.FetchOneById<Model>(modelID);
+
+			if (model != null)
+			{
+				await model.DeleteAsync();
+
+				return RedirectToAction("HandleModels", "Model");
+			}
+
+			return NotFound();
 		}
 	}
 }
