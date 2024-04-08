91e94696a6211be757c911da53b7a837f0e80212
Johanna Sievert <tinnertrasan@gmail.com>
2024-04-08 10:56:46 +0200

    Validering, bild, material, modell
    Väntar bara på kundkoppling

--- Models/OrderViewModel.cs
+++ Models/OrderViewModel.cs
@@ -1,10 +1,13 @@
 ﻿using App.Entities;
+using System.ComponentModel.DataAnnotations;
 using System.Transactions;
 
 namespace App.Models
 {
     public class OrderViewModel
     {
+        [Required(ErrorMessage = "You need to enter your headsize")]
+        [Range(40, 80, ErrorMessage = "Your head headsize must be between 40 and 80 cm.")]
         public double Size { get; set; }
         public string ModelID { get; set; }
 
