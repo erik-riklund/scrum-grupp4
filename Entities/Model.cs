e553988c699423ca64facb13992cf532b5736908
Erik R <erik.riklund@outlook.com>
2024-04-08 12:11:10 +0200

    Reapply "Merge branch 'master' into CalleAlexInkommandeOrder"
    This reverts commit 957182cf6827f7b5906bcd8f7d7ef74269d2b3e9.

--- Entities/Model.cs
+++ Entities/Model.cs
@@ -2,23 +2,23 @@
 
 namespace App.Entities
 {
-	public class Model : Entity
-	{
-		public string ModelName { get; set; }
-		public string Description { get; set; }
-		public string Picture { get; set; }
-		public string ProductCode { get; set; }
+  public class Model : Entity
+  {
+    public string ModelName { get; set; }
+    public string Description { get; set; }
+    public string Picture { get; set; }
+    public string ProductCode { get; set; }
 
-		[OwnerSide]
-		public Many<Material, Model> Materials { get; set; }
+    [OwnerSide]
+    public Many<Material, Model> Materials { get; set; }
 
-		[OwnerSide]
-		public Many<Hat, Model> Hats { get; set; }
+    [OwnerSide]
+    public Many<Hat, Model> Hats { get; set; }
 
-		public Model()
-		{
-			this.InitManyToMany(() => Materials, material => material.Models);
-			this.InitOneToMany(() => Hats);
-		}
+    public Model()
+    {
+      this.InitManyToMany(() => Materials, material => material.Models);
+      this.InitOneToMany(() => Hats);
     }
+  }
 }
