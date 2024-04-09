aeac28ea20f9d0bcedb45de41ddd82d54fdef688
Axel Österlund <132377178+AxelOsterlund1@users.noreply.github.com>
2024-04-09 14:40:08 +0200

    Update print pdf

--- Models/PrintController.cs
+++ Models/PrintController.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PdfSharp.Drawing;
 using PdfSharp.Pdf;
+using System.IO;
+using System.Reflection.Metadata;
+
 
 namespace App.Models
 {
     public class PrintController : Controller
     {
-  
+
         //public void GeneratePDFOrder(string filePath, string name, string email, string address)
         //{
         //    // Create a new PDF document
@@ -32,7 +35,7 @@ namespace App.Models
         //    // Close the document
         //    document.Close();
         //}
-        public void GeneratePDFShipping(PrintOrderViewModel model, App.Entities.Order order, App.Entities.User user)
+        public static MemoryStream GeneratePDFShipping(PrintOrderViewModel model, App.Entities.Order order, App.Entities.User user)
         {
             // Create a new PDF document
             PdfDocument document = new PdfDocument();
@@ -51,20 +54,45 @@ namespace App.Models
             gfx.DrawString($"Name: {user.FirstName + " " + user.LastName}", font, XBrushes.Black, new XPoint(50, 50));
             gfx.DrawString($"Email: {user.Email}", font, XBrushes.Black, new XPoint(50, 70));
             gfx.DrawString($"Phone: {user.PhoneNumber}", font, XBrushes.Black, new XPoint(50, 50));
-            gfx.DrawString($"Address: {user.Address.StreetAddress +" " +
-                user.Address.ZipCode +" " + user.Address.City + " " + user.Address.Country}", font, XBrushes.Black, new XPoint(50, 90));
+            gfx.DrawString($"Address: {user.Address.StreetAddress + " " +
+                user.Address.ZipCode + " " + user.Address.City + " " + user.Address.Country}", font, XBrushes.Black, new XPoint(50, 90));
             gfx.DrawString($"Sender: {""}", font, XBrushes.Black, new XPoint(50, 50));
 
             gfx.DrawString($"PackageSize: {model.PackageSize}", font, XBrushes.Black, new XPoint(50, 50));
 
 
 
+            MemoryStream stream = new MemoryStream();
+            document.Save(stream, false);
+            stream.Position = 0;
 
-            // Save the PDF document to a file
-            //document.Save(filePath);
 
             // Close the document
             document.Close();
+
+            return stream;
+        }
+
+        protected void DownloadPDF_Click(PdfDocument document)
+        {
+            MemoryStream pdfStream = new MemoryStream();
+            DownloadPDF(pdfStream);
+
+
+
+        }
+
+        protected FileContentResult DownloadPDF(MemoryStream pdfStream)
+        {
+            // Skicka PDF-dokumentet till klienten för nedladdning
+           
+
+            return new FileContentResult(pdfStream.ToArray(), "application/pdf")
+            {
+                FileDownloadName = "MyPdf.pdf"
+            };
+
+
         }
     }
 }
