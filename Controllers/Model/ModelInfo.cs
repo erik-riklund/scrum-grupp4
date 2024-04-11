828757ec27919d8dba7d67b8c42bfb89194b798a
alexmelke <alexander.melke1@outlook.com>
2024-04-11 12:06:01 +0200

    Enskild modelsida

--- Controllers/Model/ModelInfo.cs
+++ Controllers/Model/ModelInfo.cs
@@ -1,8 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using App.Entities;
 
 namespace App.Controllers
 {
     public partial class ModelController : Controller
     {
+        public async Task<IActionResult> ModelInfo(String modelID) 
+        {
+            var model = await Query.FetchOneById<Model>(modelID);
+            return View(model);
+        }
     }
 }
