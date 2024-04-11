d8a47073bfdd665347b032b6787763f2b876b63c
FannyEmberg <132469949+FannyEmberg@users.noreply.github.com>
2024-04-11 13:30:04 +0200

    Beställa material
    Beställa material

---
+++ Controllers/Order/Material.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using App.Models;
+using App.Entities;
+using MongoDB.Entities;
+using MongoDB.Driver;
+using System.Collections.Generic;
+
+namespace App.Controllers
+{
+    public partial class OrderController : Controller
+    {
+        public async Task<IActionResult> OrderMaterial()
+        {
+            var orderMaterialViewModel = new OrderMaterialViewModel();
+
+            var materialCursor = await Query.FetchAll<Material>();
+            var materialList = materialCursor.ToList();
+            ViewBag.CurrentMaterials = materialList;
+
+            
+            var materialSupplier = new Dictionary<Material, Supplier>();
+
+
+            foreach (var material in materialList)
+            {
+                var supplier = await GetSupplierForMaterial(material.SupplierID); 
+                materialSupplier[material] = supplier;
+            }
+
+            ViewBag.MaterialSupplierMap = materialSupplier;
+
+            return View(orderMaterialViewModel);
+        }
+
+        private async Task<Supplier> GetSupplierForMaterial(string materialId)
+        {
+
+            var material = await Query.FetchOneById<Supplier>(materialId);
+            if (material != null)
+            {
+                return material;
+            }
+            return null;
+
+
+        }
+    }
+
+
+ }
---
+++ Models/OrderMaterialViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace App.Models
+{
+    public class OrderMaterialViewModel
+    {
+        public string MaterialID { get; set; }
+        public string MaterialName { get; set; }
+        public string SupplierName { get; set; }
+        [Required(ErrorMessage = "You need to enter the amount in Meters")]
+        public double MaterialLenght { get; set; }
+        public string Name { get; set; } = null!;
+        public string TelephoneNumber { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Address { get; set; } = null!;
+    }
+}
