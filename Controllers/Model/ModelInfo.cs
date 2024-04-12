a09231014a9bc9445e411b11feafe7ec09f6c402
Johanna Sievert <tinnertrasan@gmail.com>
2024-04-12 13:36:01 +0200

    pricefrom

--- Controllers/Model/ModelInfo.cs
+++ Controllers/Model/ModelInfo.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using App.Entities;
 using App.Models;
+using App.Handlers;
 
 namespace App.Controllers
 {
@@ -11,6 +12,20 @@ namespace App.Controllers
             var model = await Query.FetchOneById<Model>(modelID);
             var hvm = new HatViewModel { modelID=modelID } ;
             ViewBag.Model = model;
+            var pricemodel = new Dictionary<Material, double>();
+            foreach (var material in model.Materials)
+            {
+                if (material.Unit.Equals("Meter"))
+                {
+                    pricemodel[material] = 40;
+                }
+                else
+                {
+                    pricemodel[material] = 1;
+                }
+
+            }
+            ViewBag.Price = new PriceHandler().GetHatPrice(pricemodel);
             return View(hvm);
         }
     }
