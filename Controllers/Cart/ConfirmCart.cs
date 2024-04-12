1a781e29cb64e066070ec5ab6ef04e106a9cff7a
Johanna Sievert <tinnertrasan@gmail.com>
2024-04-12 13:08:56 +0200

    Update ConfirmCart.cs

--- Controllers/Cart/ConfirmCart.cs
+++ Controllers/Cart/ConfirmCart.cs
@@ -7,23 +7,31 @@ namespace App.Controllers
     public partial class CartController : Controller
     {
         [HttpPost]
-        public async Task<IActionResult> ConfirmCart(CartViewModel hats)
+        public async Task<IActionResult> ConfirmCart()
         {
+            var customer = await session.GetUserAsync();
+            var shoppingCarts = await Query.FetchAll<Cart>();
+            var shoppingCart = shoppingCarts.Where(c => c.UserID == customer.ID).FirstOrDefault();
             var order= new Entities.Order();
             await order.SaveAsync();
             order.OrderSum = 0;
-            foreach (var hat in hats.hats)
+            foreach (var hat in shoppingCart.Hats)
             {
                 await order.Hats.AddAsync(hat);
                 order.OrderSum += hat.Price;
             }
-            var customer = await session.GetUserAsync();
+            
             order.CustomerID = customer.ID;
             DateTime today = DateTime.Today;
             DateTime futureDate = today.AddDays(14);
             order.EstimatedDeliveryDate = futureDate;
             order.Status = "Pending";
             await order.SaveAsync();
+            shoppingCart.TotalSum = 0;
+            foreach (var orderdhat in shoppingCart.Hats)
+            {
+                shoppingCart.Hats.RemoveAsync(orderdhat);
+            }
 
 
             return RedirectToAction("Confirm", "Order", new {id=order.ID});
