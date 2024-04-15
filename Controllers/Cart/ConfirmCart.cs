933981d6194a1fef84a01901eab969ee95c93427
Johanna Sievert <tinnertrasan@gmail.com>
2024-04-15 10:47:30 +0200

    confirm

--- Controllers/Cart/ConfirmCart.cs
+++ Controllers/Cart/ConfirmCart.cs
@@ -15,17 +15,22 @@ namespace App.Controllers
             var order= new Entities.Order();
             await order.SaveAsync();
             order.OrderSum = 0;
+            bool specialHat = false;
             foreach (var hat in shoppingCart.Hats)
             {
                 await order.Hats.AddAsync(hat);
                 order.OrderSum += hat.Price;
+                if(hat.Model.ModelName.Equals("Specialhat"))
+                {
+                    specialHat = true;
+                }
             }
             
             order.CustomerID = customer.ID;
             DateTime today = DateTime.Today;
             DateTime futureDate = today.AddDays(14);
             order.EstimatedDeliveryDate = futureDate;
-            order.Status = "Pending";
+            
             await order.SaveAsync();
             shoppingCart.TotalSum = 0;
             foreach (var orderdhat in shoppingCart.Hats)
@@ -33,7 +38,14 @@ namespace App.Controllers
                 shoppingCart.Hats.RemoveAsync(orderdhat);
             }
 
-
+            if(specialHat)
+            {
+                order.Status = "Pending";
+                await order.SaveAsync();
+                return RedirectToAction("ConfirmSpecial", "Order", new { id = order.ID });
+            }
+            order.Status = "Confirmed";
+            await order.SaveAsync();
             return RedirectToAction("Confirm", "Order", new {id=order.ID});
         }
     }
