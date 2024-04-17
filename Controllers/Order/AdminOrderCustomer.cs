d1c0161f30b34c4e0c0c62a770e9fb14d68afffa
Kasper <kasperv98@gmail.com>
2024-04-17 14:09:17 +0200

    shits not working

--- Controllers/Order/AdminOrderCustomer.cs
+++ Controllers/Order/AdminOrderCustomer.cs
@@ -7,15 +7,106 @@ using MongoDB.Entities;
 using MongoDB.Driver.Linq;
 using System.Linq;
 using System.Reflection;
+using App.Handlers;
+using System.Diagnostics.CodeAnalysis;
 
 namespace App.Controllers
-{ 
+{
     public partial class OrderController : Controller
     {
         [HttpGet]
-        public async Task<IActionResult> OrderCustomer()
+        public async Task<IActionResult> AdminOrderCustomer()
         {
+            var models = await Query.FetchAll<Model>();
+            List<Model> modelList = models.Where(m => m.ModelName != "Specialhat").ToList();
+            return View(modelList);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> HatOrderForCustomer(string modelID)
+        {
+            var model = await Query.FetchOneById<Model>(modelID);
+            var hvm = new HatViewModel { modelID = modelID };
+            ViewBag.Model = model;
+            var pricemodel = new Dictionary<Material, double>();
+
+            foreach (var material in model.Materials)
+            {
+                if (material.Unit.Equals("Meter"))
+                {
+                    pricemodel[material] = 40;
+                }
+                else
+                {
+                    pricemodel[material] = 1;
+                }
+
+            }
+            ViewBag.Price = new PriceHandler().GetHatPrice(pricemodel);
+
+            return View(hvm);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AdminCustomerOrderInfo(string modelID, double size)
+        {
+            var model = await Query.FetchOneById<Model>(modelID);
+            ViewBag.SelectedModel = model;
+            ViewBag.Size = size;
+
+            var priceHandler = new PriceHandler();
+
+            var materials = model.Materials.ToList();
+            Dictionary<Material, double> dictionary = new Dictionary<Material, double>();
+
+            foreach (var material in model.Materials)
+            {
+                dictionary.Add(material, size);
+            }
+
+            ViewBag.Price = priceHandler.GetHatPrice(dictionary);
+
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> PlaceOrderForCustomer(OrderCustomerViewModel orderCustomerViewModel, string modelID, double size, double price)
+        {
+            var newAddress = new Address
+            {
+                StreetAddress = orderCustomerViewModel.User.Address.StreetAddress,
+                ZipCode = orderCustomerViewModel.User.Address.ZipCode,
+                City = orderCustomerViewModel.User.Address.City,
+                Country = orderCustomerViewModel.User.Address.Country
+            };
+
+            await newAddress.SaveAsync();
+
+            var newUser = new User
+            {
+                FirstName = orderCustomerViewModel.User.FirstName,
+                LastName = orderCustomerViewModel.User.LastName,
+                Email = orderCustomerViewModel.User.Email,
+                PhoneNumber = orderCustomerViewModel.User.PhoneNumber,
+                Address = newAddress
+            };
+
+            await newUser.SaveAsync();
+
+            var newOrder = new App.Entities.Order
+            {
+                CustomerID = newUser.ID,
+                OrderDate = DateTime.Now,
+                EstimatedDeliveryDate = DateTime.Now.AddDays(14),
+                IsApproved = true,
+                Status = "Confirmed",
+                OrderSum = price
+            };
+
+            await newOrder.SaveAsync();
+
+            return View(newOrder);
+        }
+
     }
 }
