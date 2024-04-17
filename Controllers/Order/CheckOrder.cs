a33414f04e8764ee6f9434127367cf505c5fec6f
Callewinnhed <132470481+Callewinnhed@users.noreply.github.com>
2024-04-17 09:55:44 +0200

    gg

--- Controllers/Order/CheckOrder.cs
+++ Controllers/Order/CheckOrder.cs
@@ -8,6 +8,7 @@ using MongoDB.Driver.Linq;
 using System.Linq;
 using System.Reflection;
 
+
 namespace App.Controllers
 { 
     public partial class OrderController : Controller
@@ -15,27 +16,13 @@ namespace App.Controllers
         [HttpGet]
         public async Task<IActionResult> ListOrders()
         {
-            // Hämta alla ogodkända ordrar
-            var unapprovedOrders = await DB.Queryable<App.Entities.Order>()
-                                            .Where(o => !o.IsApproved)
-                                            .ToListAsync();
+          var allOrders = await Query.FetchAll<Entities.Order>();
 
-            // Hämta alla godkända ordrar
-            var approvedOrders = await DB.Queryable<App.Entities.Order>()
-                                            .Where(o => o.IsApproved)
-                                            .ToListAsync();
 
-            // Skapa en instans av ApproveOrderViewModel och skicka med listorna till vyn
-            var viewModel = new ApproveOrderViewModel
-            {
-                unapprovedOrders = unapprovedOrders,
-                approvedOrders = approvedOrders
-            };
-
-            return View(viewModel);
+            return View(allOrders.ToList());
         }
 
-        [HttpPost]
+        [HttpGet]
         public async Task<IActionResult> ApproveOrder(string orderId)
         {
             var order = await DB.Find<App.Entities.Order>().OneAsync(orderId);
@@ -49,25 +36,11 @@ namespace App.Controllers
             order.IsApproved = true;
             await order.SaveAsync();
 
-            // Hämta alla ogodkända ordrar
-            var unapprovedOrders = await DB.Queryable<App.Entities.Order>()
-                                            .Where(o => !o.IsApproved)
-                                            .ToListAsync();
+            var allOrders = await Query.FetchAll<Entities.Order>();
 
-            // Hämta alla godkända ordrar
-            var approvedOrders = await DB.Queryable<App.Entities.Order>()
-                                            .Where(o => o.IsApproved)
-                                            .ToListAsync();
 
-            // Skapa en instans av ApproveOrderViewModel och skicka med listorna till vyn
-            var viewModel = new ApproveOrderViewModel
-            {
-                unapprovedOrders = unapprovedOrders,
-                approvedOrders = approvedOrders
-            };
-
-            // Återvänd till samma vy med den uppdaterade datan
-            return View("ListOrders", viewModel);
+            
+            return View("ListOrders", allOrders.ToList());
         }
         [HttpPost]
         public async Task<IActionResult> DeclineOrder(string orderId)
