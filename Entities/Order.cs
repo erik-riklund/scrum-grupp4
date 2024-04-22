cd1277bc2756d6dab8bb6a5de0864e8367655d35
Gopher <128172895+erik-riklund@users.noreply.github.com>
2024-04-22 11:04:02 +0200

    Update Order.cs
    Signed-off-by: Gopher <128172895+erik-riklund@users.noreply.github.com>

--- Entities/Order.cs
+++ Entities/Order.cs
@@ -2,34 +2,34 @@
 
 namespace App.Entities
 {
-    public class Order : Entity
+  public class Order : Entity
+  {
+    public string CustomerID { get; set; } = null!;
+
+    public DateTime OrderDate { get; set; } = DateTime.Now;
+
+    public DateTime EstimatedDeliveryDate { get; set; }
+
+    public DateTime DeliveryDate { get; set; }
+
+    public bool IsApproved { get; set; }
+
+    public string Status { get; set; } = null!;
+
+    public double OrderSum { get; set; }
+
+    public byte[] PDF { get; set; } = null!;
+
+    [OwnerSide]
+    public Many<Hat, Order> Hats { get; set; } = null!;
+
+    [OwnerSide]
+    public Many<Shipping, Order> Shippings { get; set; } = null!;
+
+    public Order()
     {
-        public string CustomerID { get; set; } = null!;
-
-        public DateTime OrderDate { get; set; } = DateTime.Now;
-
-        public DateTime EstimatedDeliveryDate { get; set; }
-
-        public DateTime DeliveryDate { get; set; }
-
-        public bool IsApproved { get; set; }
-
-        public string Status { get; set; } = null!;
-
-        public double OrderSum { get; set; }
-
-        public byte[] PDF { get; set; } = null!;
-
-        [OwnerSide]
-        public Many<Hat, Order> Hats { get; set; } = null!;
-
-        [OwnerSide]
-        public Many<Shipping, Order> Shippings { get; set; } = null!;
-
-        public Order()
-        {
-            this.InitOneToMany(() => Hats);
-            this.InitOneToMany(() => Shippings);
-        }
+      this.InitOneToMany(() => Hats);
+      this.InitOneToMany(() => Shippings);
     }
+  }
 }
