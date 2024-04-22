abbd4ef886ecf5d7d320598a70adf00edcd60be5
LarssonMarcus <132892741+LarssonMarcus@users.noreply.github.com>
2024-04-22 11:01:23 +0200

    Update Order.cs

--- Entities/Order.cs
+++ Entities/Order.cs
@@ -2,36 +2,34 @@
 
 namespace App.Entities
 {
-  public class Order : Entity
-  {
-    public string CustomerID { get; set; } = null!;
+    public class Order : Entity
+    {
+        public string CustomerID { get; set; } = null!;
 
-    public DateTime OrderDate { get; set; }
+        public DateTime OrderDate { get; set; } = DateTime.Now;
 
-    public DateTime EstimatedDeliveryDate { get; set; }
+        public DateTime EstimatedDeliveryDate { get; set; }
 
-    public DateTime DeliveryDate { get; set; }
+        public DateTime DeliveryDate { get; set; }
 
-    public bool IsApproved { get; set; }
+        public bool IsApproved { get; set; }
 
-    public string Status { get; set; } = null!;
+        public string Status { get; set; } = null!;
 
-    public double OrderSum { get; set; }
+        public double OrderSum { get; set; }
 
-    public byte[] PDF { get; set; } = null!;
-
-    public bool PayStatus { get; set; } = false;
+        public byte[] PDF { get; set; } = null!;
 
         [OwnerSide]
-    public Many<Hat, Order> Hats { get; set; } = null!;
+        public Many<Hat, Order> Hats { get; set; } = null!;
 
-    [OwnerSide]
-    public Many<Shipping, Order> Shippings { get; set; } = null!;
+        [OwnerSide]
+        public Many<Shipping, Order> Shippings { get; set; } = null!;
 
-    public Order()
-    {
-      this.InitOneToMany(() => Hats);
-      this.InitOneToMany(() => Shippings);
+        public Order()
+        {
+            this.InitOneToMany(() => Hats);
+            this.InitOneToMany(() => Shippings);
+        }
     }
-  }
 }
